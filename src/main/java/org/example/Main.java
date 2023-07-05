2bcf08d013c59bef27e10b56fe71a0a5a5397270
Gautier <gautier.edel.info@gmail.com>
2023-07-05 15:34:02 +0200

    google cloud deployment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        int port = 8080; // Set the port number
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/hello", (exchange -> {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(response.getBytes());
+            outputStream.close();
+        }));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        server.start();
+
+        System.out.println("Server started on port " + port);
     }
 }
