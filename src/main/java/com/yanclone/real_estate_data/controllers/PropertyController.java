f50dac1d7f6afc1a96368ad13d9c9dc1ab043b2b
Gautier <gautier.edel.info@gmail.com>
2023-07-06 19:23:10 +0200

    Request error handling

--- src/main/java/com/yanclone/real_estate_data/controllers/PropertyController.java
+++ src/main/java/com/yanclone/real_estate_data/controllers/PropertyController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import org.jetbrains.annotations.NotNull;
 
 public class PropertyController implements HttpHandler {
     private final PropertyService propertyService;
@@ -26,60 +25,70 @@ public class PropertyController implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
         String response = "";
+        int statusCode = 200;
 
         // Convert properties to JSON format
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-        if (exchange.getRequestURI().getPath().endsWith("/properties") && method.equalsIgnoreCase("GET")) {
-            List<Property> properties = propertyService.getAllProperties();
-            response = objectMapper.writeValueAsString(properties);
-            exchange.getResponseHeaders().set("Content-Type", "application/json");
-        } else if (exchange.getRequestURI().getPath().endsWith("/property")) {
-            if (method.equalsIgnoreCase("POST")) {
-                Property newProperty = getPropertyFromRequest(exchange);
-                Property createdProperty = propertyService.createProperty(newProperty);
-
-                response = objectMapper.writeValueAsString(createdProperty);
-                exchange.getResponseHeaders().set("Content-Type", "application/json");
-            } else if (method.equalsIgnoreCase("GET")) {
-                String propertyId = extractPropertyIdFromRequest(exchange);
-
-                Property retrievedProperty = propertyService.getPropertyById(propertyId);
-
-                if (retrievedProperty != null) {
-                    response = objectMapper.writeValueAsString(retrievedProperty);
-                    exchange.getResponseHeaders().set("Content-Type", "application/json");
+        try {
+            if (exchange.getRequestURI().getPath().endsWith("/properties")) {
+                if (method.equalsIgnoreCase("GET")) {
+                    List<Property> properties = propertyService.getAllProperties();
+                    response = objectMapper.writeValueAsString(properties);
                 } else {
-                    response = "Property not found";
+                    response = "Invalid method for /properties endpoint";
+                    statusCode = 400;
                 }
-            } else if (method.equalsIgnoreCase("PUT")) {
-                String propertyId = extractPropertyIdFromRequest(exchange);
-                Property updatedProperty = getPropertyFromRequest(exchange);
-
-                Property updated = propertyService.updateProperty(propertyId, updatedProperty);
-
-                if (updated != null) {
-                    response = objectMapper.writeValueAsString(updated);
-                    exchange.getResponseHeaders().set("Content-Type", "application/json");
+            } else if (exchange.getRequestURI().getPath().endsWith("/property")) {
+                if (method.equalsIgnoreCase("POST")) {
+                    Property newProperty = getPropertyFromRequest(exchange);
+                    Property createdProperty = propertyService.createProperty(newProperty);
+                    response = objectMapper.writeValueAsString(createdProperty);
+                } else if (method.equalsIgnoreCase("GET")) {
+                    String propertyId = extractPropertyIdFromRequest(exchange);
+                    Property retrievedProperty = propertyService.getPropertyById(propertyId);
+                    if (retrievedProperty != null) {
+                        response = objectMapper.writeValueAsString(retrievedProperty);
+                    } else {
+                        response = "Property not found";
+                        statusCode = 404;
+                    }
+                } else if (method.equalsIgnoreCase("PUT")) {
+                    String propertyId = extractPropertyIdFromRequest(exchange);
+                    Property updatedProperty = getPropertyFromRequest(exchange);
+                    Property updated = propertyService.updateProperty(propertyId, updatedProperty);
+                    if (updated != null) {
+                        response = objectMapper.writeValueAsString(updated);
+                    } else {
+                        response = "Failed to update property";
+                        statusCode = 400;
+                    }
+                } else if (method.equalsIgnoreCase("DELETE")) {
+                    String propertyId = extractPropertyIdFromRequest(exchange);
+                    boolean deleted = propertyService.deleteProperty(propertyId);
+                    if (deleted) {
+                        response = "Property deleted";
+                    } else {
+                        response = "Failed to delete property";
+                        statusCode = 400;
+                    }
                 } else {
-                    response = "Failed to update property";
+                    response = "Invalid method for /property endpoint";
+                    statusCode = 400;
                 }
-            } else if (method.equalsIgnoreCase("DELETE")) {
-                String propertyId = extractPropertyIdFromRequest(exchange);
-
-                boolean deleted = propertyService.deleteProperty(propertyId);
-
-                if (deleted) {
-                    response = "Property deleted";
-                } else {
-                    response = "Failed to delete property";
-                }
+            } else {
+                response = "Invalid endpoint";
+                statusCode = 404;
             }
+        } catch (Exception e) {
+            response = "An error occurred: " + e.getMessage();
+            statusCode = 500;
         }
 
 
-        exchange.sendResponseHeaders(200, response.getBytes().length);
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(statusCode, response.getBytes().length);
         OutputStream outputStream = exchange.getResponseBody();
         outputStream.write(response.getBytes());
         outputStream.close();
